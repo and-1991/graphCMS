4497cc81a232a6678e7fb02fbfb0361aab6ee301
Yuriy <and-1991@list.ru>
2022-01-17 14:10:33 +0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
+  i18n: {
+    locales: ['en', 'ru'],
+    defaultLocale: 'en',
+    localeDetection: false,
+  },
+  trailingSlash: true,
   reactStrictMode: true,
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
+import Image from 'next/image'
+
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const Header = () => {
+  return (
+    <div style={{display: 'flex', width: '100%', backgroundColor: '#0176fe', padding: '10px 20px'}}>
+      <Image
+        src="/images/logo.svg"
+        alt="logo"
+        width={150}
+        height={50}
+      />
+    </div>
+  )
+}
+
+function MyApp({Component, pageProps}) {
+  return (
+    <div>
+      <Header/>
+      <Component {...pageProps}/>
+    </div>
+  )
 }
 
 export default MyApp
